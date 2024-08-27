a434754893a29aa77fdb7caef83c1890337bdee8
Bernardo Brumer <137068913+bernardobrumer@users.noreply.github.com>
2024-08-27 19:36:19 -0300

    Add files via upload

---
+++ a_main_04.py
@@ -0,0 +1,34 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+# import awsgi
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4('Analysis of Iris data using scatter matrix'),
+    dcc.Dropdown(
+        id="dropdown",
+        options=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
+        value=['sepal_length', 'sepal_width'],
+        multi=True
+    ),
+    dcc.Graph(id="graph"),
+])
+
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("dropdown", "value"))
+def update_bar_chart(dims):
+    df = px.data.iris() # replace with your own data source
+    fig = px.scatter_matrix(
+        df, dimensions=dims, color="species")
+    return fig
+
+
+# def lambda_handler(event, context):
+#     return awsgi.response(app, event, context, base64_content_types={"image/png"})
+
+
+if __name__ == '__main__':
+    app.run()
